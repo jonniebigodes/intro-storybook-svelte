4e6fda68bc0a5cb357d93cf15d5c91a3ac6fb93d
jonniebigodes <jonniebigodes@hotmail.com>
2019-11-06 00:11:46 +0000

    create addons final code and assets

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,5 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register'; // knobs addon section
 import '@storybook/addon-links/register';
+
+import "./addons/design-assets";
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -57,7 +57,7 @@ storiesOf("Task", module)
   }); */
 
 // updated code for knobs addon
-import { storiesOf } from "@storybook/svelte";
+/* import { storiesOf } from "@storybook/svelte";
 import { action } from "@storybook/addon-actions";
 import { withKnobs, object } from "@storybook/addon-knobs";
 import Task from "./Task.svelte";
@@ -76,6 +76,163 @@ export const actions = {
 
 storiesOf("Task", module)
   .addDecorator(withKnobs)
+  .add("default", () => {
+    return {
+      Component: Task,
+      props: {
+        task: object("task", { ...task })
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("pinned", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          state: "TASK_PINNED"
+        }
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("archived", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          state: "TASK_ARCHIVED"
+        }
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("longTitle", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          title: longTitle
+        }
+      }
+    };
+  }); */
+
+// updated for creating addons section
+/* import { storiesOf } from "@storybook/svelte";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs";
+import Task from "./Task.svelte";
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+export const task = {
+  id: "1",
+  title: "Test Task",
+  state: "Task_INBOX",
+  updated_at: new Date(2019, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    assets: [
+      "path/to/your/asset.png",
+      "path/to/another/asset.png",
+      "path/to/yet/another/asset.png"
+    ]
+  })
+  .add("default", () => {
+    return {
+      Component: Task,
+      props: {
+        task: object("task", { ...task })
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("pinned", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          state: "TASK_PINNED"
+        }
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("archived", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          state: "TASK_ARCHIVED"
+        }
+      },
+      on: {
+        ...actions
+      }
+    };
+  })
+  .add("longTitle", () => {
+    return {
+      Component: Task,
+      props: {
+        task: {
+          ...task,
+          title: longTitle
+        }
+      }
+    };
+  });
+ */
+
+ //final code
+ import { storiesOf } from "@storybook/svelte";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs";
+import Task from "./Task.svelte";
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+export const task = {
+  id: "1",
+  title: "Test Task",
+  state: "Task_INBOX",
+  updated_at: new Date(2019, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    assets: [
+      "./image_1.jpg",
+      "./image_2.jpg",
+      "./image_3.jpg"
+    ]
+  })
   .add("default", () => {
     return {
       Component: Task,
