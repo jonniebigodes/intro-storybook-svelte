2b9a516e94d1ccd3a130f2975a16efdd634cadd8
jonniebigodes <jonniebigodes@hotmail.com>
2019-12-03 23:53:50 +0000

    removed one line to prevent duplication of task

--- src/components/PureTaskList.stories.js
+++ src/components/PureTaskList.stories.js
@@ -181,7 +181,6 @@ export const defaultTasksData = [
 ];
 export const withPinnedTasksData = [
   ...defaultTasksData.slice(0, 5),
-  { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
   { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" }
 ];
 // default TaskList state
